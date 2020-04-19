81efaf38d0d6f4ae30d992093eb81be7fc25dbe4
SumeyyaSabanci <ssabanci@gmail.com>
2020-04-19 16:37:29 -0500

    new commit

--- src/Q_85_127/Q101.java
+++ src/Q_85_127/Q101.java
@@ -8,20 +8,20 @@ abstract class Planet{
     }
     abstract void rotate();
 }
-//class Earth extends Planet{
-//    private void revolve(){
-//
-//    }
-//    private void rotate(){
-//
-//    }
-//}
+class Earth extends Planet{
+    public void revolve(){
+
+    }
+    protected void rotate(){
+
+    }
+}
 /*
-Which two modifications enable the code to compile?
+Which two modifications enable the code to compile? or question might be three modifications
 A. Make the method at line 8 protected.
 B.	Make the method at line 8 public.
 C.	Make the method at line 10 protected.
 D.	Make the method at line 4 public.
 E.	Make the method at line 2 public.
-Answer: ABC
+Answer: AC veya BC
  */
